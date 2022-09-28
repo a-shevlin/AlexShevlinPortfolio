aa09aed222617c74d68b75b8087feaf8ac3f5a03
Alex Shevlin <alexshevlin1@gmail.com>
2022-09-27 23:59:32 -0700

    place excess html in child components.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,33 +4,18 @@ import About from './About';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode, faGamepad } from '@fortawesome/free-solid-svg-icons';
 import LinkedIn from '../img/linkedin.png';
-import slag from '../img/slag.png'
 
 function NavBar() {
 
   return (
     <React.Fragment>
-      <div class="navbar">
-        <Projects>
-          <h1>My Projects</h1>
-          <hr/>
-          <h5>SLAG Game</h5>
-          <em><p>C#, MVC, CSS, Razor</p></em>
-          <img src={slag}/>
-          <a href="https://github.com/a-shevlin" target="_blank" class="slag">
-            <FontAwesomeIcon icon={faCode} />
-          </a>
-          <a href="https://www.linkedin.com/in/alexshevlin/" target="_blank" class="gameLink">
-            <FontAwesomeIcon icon={faGamepad} />
-          </a>
-        </Projects>
-        <About>
-          <h1>About Me</h1>
-        </About>
-        <a href="https://github.com/a-shevlin" target="_blank" class="github">
+      <div className="navbar">
+        <Projects />
+        <About />
+        <a href="https://github.com/a-shevlin" target="_blank" className="github">
           <FontAwesomeIcon icon={faCode} />
         </a>
-        <a href="https://www.linkedin.com/in/alexshevlin/" target="_blank" class="linkedin">
+        <a href="https://www.linkedin.com/in/alexshevlin/" target="_blank" className="linkedin">
           <img src={LinkedIn}/>
         </a>
         <a href="#">
