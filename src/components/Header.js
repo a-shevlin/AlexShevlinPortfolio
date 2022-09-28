b65b828e12e80b59f123ee9f0c2dc095c60f3e15
Alex Shevlin <alexshevlin1@gmail.com>
2022-09-27 23:59:44 -0700

    update to className.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <React.Fragment>
-      <div class="header">
+      <div className="header">
         <h1 id="name">Alex Shevlin</h1>
       </div>
     </React.Fragment>
