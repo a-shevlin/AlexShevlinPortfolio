ce3e2c0c8febbdc4dc3747d010f28900a0bedfc2
Alex Shevlin <alexshevlin1@gmail.com>
2022-09-28 16:07:23 -0700

    add to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ function Header() {
     <React.Fragment>
       <div className="header">
         <h1 id="name">Alex Shevlin</h1>
+        <p>FULL STACK DEVELOPER</p>
       </div>
     </React.Fragment>
   );
